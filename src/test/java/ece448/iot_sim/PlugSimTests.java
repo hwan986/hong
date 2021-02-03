580d41dbee8c8705b75f2d6488775af861085e23
Changhwan <chong12@hawk.iit.edu>
2021-02-03 01:38:20 -0600

    unit test project 1 passed

--- src/test/java/ece448/iot_sim/PlugSimTests.java
+++ src/test/java/ece448/iot_sim/PlugSimTests.java
@@ -21,4 +21,126 @@ public class PlugSimTests {
 
 		assertTrue(plug.isOn());
 	}
+	@Test
+	public void testName1() {
+		PlugSim plug = new PlugSim("a");
+
+		assertEquals("a",plug.getName());
+	}
+
+	@Test
+	public void testName2() {
+		PlugSim plug = new PlugSim("a.100");
+
+		assertEquals("a.100",plug.getName());
+	}
+
+
+	@Test
+	public void testSwitchOff() {
+		PlugSim plug = new PlugSim("a");
+
+		plug.switchOff();
+		
+		assertFalse(plug.isOn());
+	}
+	@Test
+	public void testSwitchOnOff1() {
+		PlugSim plug = new PlugSim("a");
+
+		plug.switchOn();
+		plug.switchOff();
+
+		assertFalse(plug.isOn());
+	}
+	@Test
+	public void testSwitchOnOff2() {
+		PlugSim plug = new PlugSim("a");
+
+		plug.switchOn();
+		plug.switchOff();
+		plug.switchOn();
+
+		assertTrue(plug.isOn());
+	}
+
+	@Test
+	public void testToggle1() {
+		PlugSim plug = new PlugSim("a");
+
+		plug.switchOn();
+		plug.switchOff();
+		plug.switchOn();
+		plug.toggle();
+
+		assertFalse(plug.isOn());
+	}
+	@Test
+	public void testToggle2() {
+		PlugSim plug = new PlugSim("a");
+
+		plug.switchOn();
+		plug.switchOff();
+		plug.switchOn();
+		plug.toggle();
+		plug.toggle();
+
+		assertTrue(plug.isOn());
+	}
+
+	@Test
+	
+	public void testPower1(){
+		PlugSim plug = new PlugSim("b.230");
+		plug.switchOn();
+		plug.measurePower();
+	
+		assertTrue(plug.getPower()==230);
+	}
+	@Test
+	
+	public void testPower2(){
+		PlugSim plug = new PlugSim("b.50");
+		plug.switchOn();
+		plug.measurePower();
+	
+		assertTrue(plug.getPower()==50);
+	}
+	@Test
+	public void testPower3(){
+		PlugSim plug = new PlugSim("b.500");
+		plug.switchOn();
+		plug.measurePower();
+	
+		assertTrue(plug.getPower()==500);
+	}
+	@Test
+	public void testPower4(){
+		PlugSim plug = new PlugSim("b.230");
+		plug.switchOn();
+		plug.switchOff();
+		plug.measurePower();
+	
+		assertTrue(plug.getPower()==0);
+	}
+
+	@Test
+	public void testPower5(){
+		PlugSim plug = new PlugSim("b.230");
+		plug.switchOff();
+		plug.measurePower();
+	
+		assertFalse(plug.isOn() );
+	}
+	@Test
+	public void testPower6(){
+		PlugSim plug = new PlugSim("b.230");
+		plug.switchOn();
+		plug.measurePower();
+	
+		assertTrue(plug.isOn() && plug.getPower()==230);
+	}
+
+
+
 }
