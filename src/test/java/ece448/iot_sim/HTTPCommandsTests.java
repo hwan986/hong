52437d150ef2b31d4c21c8408944341a391410b9
Changhwan <chong12@hawk.iit.edu>
2021-02-18 21:58:40 -0600

    Unit Test2

--- src/test/java/ece448/iot_sim/HTTPCommandsTests.java
+++ src/test/java/ece448/iot_sim/HTTPCommandsTests.java
@@ -24,6 +24,43 @@ public class HTTPCommandsTests {
 		//assertTrue(cmds.handleGet("/a", new HashMap<>()) != null);
 	}
 
+	@Test
+	public void test2() {
+		PlugSim a = new PlugSim("a");
+		PlugSim x = new PlugSim("x");
+
+		ArrayList<PlugSim> list = new ArrayList<>();
+		list.add(a);
+		list.add(x);
+
+		HTTPCommands cmds = new HTTPCommands(list);
+
+		assertEquals(cmds.handleGet("/", new HashMap<>()), "<html><body><p><a href='/a'>a</a></p><p><a href='/x'>x</a></p></body></html>");
+		//assertTrue(cmds.handleGet("/a", new HashMap<>()) != null);
+	}
+
+
+	
+@Test
+	public void test3() {
+		PlugSim a = new PlugSim("a");
+		PlugSim x = new PlugSim("x");
+
+		ArrayList<PlugSim> list = new ArrayList<>();
+		list.add(a);
+		list.add(x);
+
+		HTTPCommands cmds = new HTTPCommands(list);
+
+		HashMap<String,String> params = new HashMap<>();
+		params.put("action", "on");
+
+		String result  = cmds.handleGet("/a", params);
+		//assertEquals(true, a.isOn());
+		assertEquals(result, "<html><body><p>Plug a is on.</p><p>Power reading is 0.000.</p><p><a href='/a?action=on'>Switch On</a></p><p><a href='/a?action=off'>Switch Off</a></p><p><a href='/a?action=toggle'>Toggle</a></p></body></html>");
+		
+	}
+
 
 
 
