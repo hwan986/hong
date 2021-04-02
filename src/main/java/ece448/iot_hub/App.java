3a2d381f87959914855cf35334db8911f05005be
Changhwan <chong12@hawk.iit.edu>
2021-04-01 22:19:32 -0500

    pr4-2

--- src/main/java/ece448/iot_hub/App.java
+++ src/main/java/ece448/iot_hub/App.java
@@ -1,12 +1,27 @@
 package ece448.iot_hub;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class App {
-	@Autowired
-	public App(Environment env) throws Exception {
+	//@Autowired
+	//public App(Environment env) throws Exception {
+	//}
+	@Bean (destroyMethod = "disconnect")
+	public MqttClient mqttClient(Environment env) throws Exception{
+		String broker = env.getProperty("mqtt.broker");
+		String clientID = env.getProperty("mqtt.clientID");
+		MqttClient mqtt = new MqttClient(broker, clientID, new MemoryPersistence());
+		mqtt.connect();
+		logger.info("MqttClient {} connected: {}", clientID, broker);
+		return mqtt;
 	}
+	
 }
